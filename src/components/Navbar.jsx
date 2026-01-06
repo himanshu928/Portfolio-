6a82b240d85350e9be7ce57153f609e8c91e8add
himanshu928 <sahuhimanshu95656@gmail.com>
2026-01-06 20:52:28 +0530

    Initial React portfolio setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import logoImage from '../assets/logo.png'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <a href=""><img src={logoImage} alt="Logo" /></a>
+        <ul>
+          <a href="AboutMe.jsx"><li>Home</li></a>
+          <a href="AboutMe.jsx"><li>About</li></a>
+          <a href="Skills.jsx"><li>Skills</li></a>
+          <a href="Portfolio.jsx"><li>Portfolio</li></a>
+          <a href="Contact.jsx"><li>Contact</li></a>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
+
